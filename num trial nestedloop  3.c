ff0361d0a8efca38083fef563af1c5cac4eafcaa
saisahir7 <95203642+saisahir7@users.noreply.github.com>
2021-12-29 10:10:24 +0530

    Update num trial nestedloop  3.c

--- num trial nestedloop  3.c
+++ num trial nestedloop  3.c
@@ -3,11 +3,11 @@ main()
 {
 	int i,j;
 	
-	for(i = 1; i <= 1; i++)
+	for(i = 1; i <= 4; i++)
 	{
-		for(j = 4; j >= i; j--)
+		for(j = 1; j <= i; j++)
 		{
-			printf("%d",i);
+			printf("%d",j);
 		}
 		printf("\n");
 	}
